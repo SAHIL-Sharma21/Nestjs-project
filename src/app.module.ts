80783e0000cd8f7d94977490280adc6e10144a16
Sahil <sahilsharma2000dtc@gmail.com>
2024-09-19 13:33:55 +0530

    added rate limitter in our project using nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule, 
+    DatabaseModule, 
+    EmployeesModule,
+    ThrottlerModule.forRoot([{
+      name: "short",
+      ttl: 60000, // 1 minutes
+      limit: 3, // 3 requestes in 1 minutes
+    }, {
+      name: 'long',
+      ttl: 600000,
+      limit: 100,
+    }])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }],
 })
 export class AppModule {}
